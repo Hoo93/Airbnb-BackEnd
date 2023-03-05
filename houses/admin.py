257456ddc2baf62220f3ee3a13122e6b96c30ad6
SangHoo Park <sksk8922@gmail.com>
2023-03-05 22:48:53 +0900

    migrate Houses app

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
+@admin.register(House)
+# admin 할 모델이 House 라고 설명해줌
+class HouseAdmin(admin.ModelAdmin):
+    pass
