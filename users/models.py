e2ec5552d0aa5a05d30b0593713111792275a0a1
SangHoo Park <sksk8922@gmail.com>
2023-03-06 01:51:34 +0900

    User 오버라이딩, Auth 설정

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# not models.Model 기본 User를 사용하는 것이 아니라
+# AbstractUser를 사용하여 우리만의 User를 Custom
+class User(AbstractUser):   
+    pass
