1aa5f8e7214acd2ceef3af44cf5d5a6cb68738cd
SangHoo Park <sksk8922@gmail.com>
2023-03-06 19:31:43 +0900

    remove Houses Apps

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
+# 이걸 import해서 userAdmin을 사용해야함
+# admin.Modeladmin을 쓰면 커스텀이 아닌 기본 설정을 쓰는 것!!
+# 그러므로 반드시 아래를 임포트 해서 커스ㅓ마이징해야함
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username", "password", "name", "email", "is_host",),
+                "classes":("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes":("collapse",),
+            },
+        ),
+        (
+            "Importat Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes":("collapse",),
+            },
+        ),
+    )
+    # fields = ("email","password","name")
+    list_display = ("username","email","name","is_host",)
+
+    pass
--- users/models.py
+++ users/models.py
@@ -4,5 +4,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 # not models.Model 기본 User를 사용하는 것이 아니라
 # AbstractUser를 사용하여 우리만의 User를 Custom
-class User(AbstractUser):   
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150,default="")
+    is_host = models.BooleanField(default=False)
+    profile_phto = models.ImageField()
+    
     pass
